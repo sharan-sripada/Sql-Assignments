c4e35fe42f5fa6156e73c44792c1e21b3f2b96fd
sharan-sripada <sharan.sripada@zemosolabs.com>
2021-07-13 17:14:16 +0530

    modified

--- Assignment-2/Q4.sql
+++ Assignment-2/Q4.sql
@@ -3,4 +3,4 @@ SELECT  category, COUNT(*) FROM film_list WHERE
     FID IN (SELECT  film_id  FROM  customer_list,rental,inventory  WHERE
             customer_list.ID = rental.customer_id AND rental.inventory_id = inventory.inventory_id
 				and customer_list.name = 'PATRICIA JOHNSON')
-GROUP BY category LIMIT 3;
+GROUP BY category ORDER BY COUNT(*) DESC LIMIT 3;
